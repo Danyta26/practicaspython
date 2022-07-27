cf1e6a127c25eb1ce6a10eddea7cf16f9350b11d
Daniela <Daniela>
2022-07-26 20:45:45 -0600

    Mis practicas de python

---
+++ cadenas.py
@@ -0,0 +1,10 @@
+# texto_variado = "Palabra 123 +-* #$&"
+# print(type(texto_variado))
+
+#Podemos utilizar comillas triples para que el texto se muestre como la cedena que hemos escrito
+# print("""
+# Funcionamiento de \
+# programa (opciones)
+# -1 Para acceder opciones
+#     -2 para salir
+#  """)
